a3af185446aad5e6a4c32b113e51284628d8fb83
STUDENT\2111010 <s2170149@s.do-johodai.ac.jp>
2023-11-11 17:31:31 +0900

    なんか変更

---
+++ Assets/Resource/Title/Script/AudioManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    AudioSource bgmAudioSource;
+    [SerializeField]
+    AudioSource seAudioSource;
+
+    public float Volume
+    {
+        get
+        {
+            return audioSource.volume;
+        }
+        set
+        {
+            audioSource.volume = Mathf.Clamp01(value);
+        }
+    }
+
+    public float BgmVolume
+    {
+        get
+        {
+            return bgmAudioSource.volume;
+        }
+        set
+        {
+            bgmAudioSource.volume = Mathf.Clamp01(value) * audioSource.volume;
+        }
+    }
+
+    public float SeVolume
+    {
+        get
+        {
+            return seAudioSource.volume;
+        }
+        set
+        {
+            seAudioSource.volume = Mathf.Clamp01(value) * audioSource.volume;
+        }
+    }
+
+    void Start()
+    {
+        GameObject soundManager = CheckOtherSoundManager();
+        bool checkResult = soundManager != null && soundManager != gameObject;
+
+        if (checkResult)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    GameObject CheckOtherSoundManager()
+    {
+        return GameObject.FindGameObjectWithTag("SoundManager");
+    }
+
+    public void PlayBgm(AudioClip clip)
+    {
+        bgmAudioSource.clip = clip;
+
+        if (clip == null)
+        {
+            return;
+        }
+
+        bgmAudioSource.Play();
+    }
+
+    public void PlaySe(AudioClip clip)
+    {
+        if (clip == null)
+        {
+            return;
+        }
+
+        seAudioSource.PlayOneShot(clip);
+    }
+}
